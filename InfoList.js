7a91718cbbaea30a464ae853afcbba7644b7eaa3
lpfjustino <lpfjustino@gmail.com>
2018-08-05 21:16:21 -0300

    Added the SortableListView

--- InfoList.js
+++ InfoList.js
@@ -1,7 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TouchableHighlight } from 'react-native';
+import SortableListView from 'react-native-sortable-listview';
 
-export default class App extends React.Component {
+let data = {
+    hello: { text: 'world' },
+    how: { text: 'are you' },
+    test: { text: 123 },
+    this: { text: 'is' },
+    a: { text: 'a' },
+    real: { text: 'real' },
+    drag: { text: 'drag and drop' },
+    bb: { text: 'bb' },
+    cc: { text: 'cc' },
+    dd: { text: 'dd' },
+    ee: { text: 'ee' },
+    ff: { text: 'ff' },
+    gg: { text: 'gg' },
+    hh: { text: 'hh' },
+    ii: { text: 'ii' },
+    jj: { text: 'jj' },
+    kk: { text: 'kk' },
+}
+
+let order = Object.keys(data) //Array of keys
+
+export default class InfoList extends React.Component {
     _onPress() {
 
     }
@@ -9,10 +32,36 @@ export default class App extends React.Component {
     render() {
         const { navigate } = this.props.navigation;
         return (
-            <View style={styles.container}>
-                <Text>Keede Lycea</Text>
-            </View>
+            <SortableListView
+                style={{ flex: 1 }}
+                data={data}
+                order={order}
+                onRowMoved={e => {
+                    order.splice(e.to, 0, order.splice(e.from, 1)[0])
+                    this.forceUpdate()
+                }}
+                renderRow={row => <RowComponent data={row} />}
+            />
         );
+    }
+}
+
+class RowComponent extends React.Component {
+    render() {
+        return (
+            <TouchableHighlight
+                underlayColor={'#eee'}
+                style={{
+                    padding: 25,
+                    backgroundColor: '#F8F8F8',
+                    borderBottomWidth: 1,
+                    borderColor: '#eee',
+                }}
+                {...this.props.sortHandlers}
+            >
+                <Text>{this.props.data.text}</Text>
+            </TouchableHighlight>
+        )
     }
 }
 
