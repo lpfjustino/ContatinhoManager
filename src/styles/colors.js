af523f0c7fcf2f038cc4ee4a6b4c3adc8288ade7
lpfjustino <lpfjustino@gmail.com>
2018-09-30 14:49:14 -0300

    Introduced new style architecture

---
+++ src/styles/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+    header: '#333333',
+    primary: '#069',
+    red: rgba(228, 87, 46, 1),
+    orange: rgba(243, 167, 18, 1),
+    yellow: rgba(242, 192, 120, 1),
+    blue: rgba(32, 67, 91, 1),
+    darkblue: rgba(0, 42, 50, 1),
+};
+
+export default colors;
