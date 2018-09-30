9a6e7af5f4d11e08b835b9278cad93c54c49531f
lpfjustino <lpfjustino@gmail.com>
2018-09-30 15:30:07 -0300

    Fixed general project config

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,11 +1,12 @@
 const colors = {
     header: '#333333',
     primary: '#069',
-    red: rgba(228, 87, 46, 1),
-    orange: rgba(243, 167, 18, 1),
-    yellow: rgba(242, 192, 120, 1),
-    blue: rgba(32, 67, 91, 1),
-    darkblue: rgba(0, 42, 50, 1),
+    background: '#F2C078',
+    red: '#E4572E',
+    orange: '#F3A712',
+    yellow: '#F2C078',
+    blue: '#20435B',
+    darkblue: '#002A32',
 };
 
 export default colors;
